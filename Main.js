78337c4e9eecae4eb29dff70b14cd101ecbd60bb
nikita <nikitacohen7@gmail.com>
2022-06-01 01:21:54 +0300

    change multi

--- Main.js
+++ Main.js
@@ -201,7 +201,7 @@ async function initLoadingArray () {
 function loadArrayQ () {
     return new Promise((resolve, reject) => {
         Promise.all(queue2.map((q, index) => {
-            if (index < 80) {
+            if (index < 50) {
                 return initThirdWorker(q, index)
             }
 
@@ -224,7 +224,7 @@ function init () {
             await initLoadingArray();
 
             workers = []
-            AMOUNT = 80;
+            AMOUNT = 50;
 
             createWorkers("./workerThread.js")
 
