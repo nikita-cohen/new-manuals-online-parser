de7c445a9786e0a13e305a9f0cff602ce0ba01be
nikita <nikitacohen7@gmail.com>
2022-06-01 15:00:30 +0300

    change multi

--- Main.js
+++ Main.js
@@ -175,7 +175,7 @@ function resetAtMidnight() {
         now.getFullYear(),
         now.getMonth(),
         now.getDate() + 1,
-        0, 0, 0
+        3, 0, 0
     );
 
     let msToMidnight = night.getTime() - now.getTime();
