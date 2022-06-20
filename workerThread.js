86c26bbf35abad548c8fce9c2e1d6265c83d761d
nikita <nikitacohen7@gmail.com>
2022-06-20 17:57:31 +0300

    change multi

--- workerThread.js
+++ workerThread.js
@@ -1,6 +1,14 @@
 const {parentPort} = require("worker_threads");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const manualSchema = require("./service/SearchService");
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/findManual-complete-four').then()
+    .catch(e => {
+        console.log(e)
+    })
 
 function getFirstData(obj) {
     return new Promise(async (resolve, reject) => {
@@ -12,10 +20,10 @@ function getFirstData(obj) {
         const hrefArray = [];
 
         for (let i = 0; i < href.length; i++) {
-            hrefArray.push(obj.url.slice(0, -1) + $(href[i]).children("a").attr('href'));
+            hrefArray.push({url : obj.url.slice(0, -1) + $(href[i]).children("a").attr('href'), type : "category"});
         }
 
-        resolve({hrefs : hrefArray, message : "done"});
+        resolve({hrefs : hrefArray, message : "done", isForDb : false});
     })
 }
 
@@ -44,11 +52,13 @@ function getSecondData(obj) {
         const elementArray = [];
 
         for (let i = 0; i < element.length; i++) {
-            elementArray.push("http://www.manualsonline.com" + $(element[i]).children("a").attr('href'));
+            elementArray.push({url : "http://www.manualsonline.com" + $(element[i]).children("a").attr('href'), type : "lastUrl"});
         }
 
-        console.log("ok")
-        resolve({hrefs : elementArray, message : "done2"});
+        console.log("here")
+        console.log(elementArray.length)
+        resolve({hrefs : elementArray, message : "done", isForDb : false});
+
     })
 
 }
@@ -84,16 +94,16 @@ function getThirdData(obj) {
             const finalObject = $("div.col-md-8.col-sm-8.col-xs-7 > h5");
 
             for (let i = 0; i < finalObject.length; i++) {
-                //console.log({id : $(finalObject[i]).children("a").text().replace(/[^a-zA-Z0-9 ]/g, '').trim().replaceAll(' ', '_') , brand, category, "url":  "http://www.manualsonline.com" + $(finalObject[i]).children("a").attr('href'), "title": $(finalObject[i]).children("a").text().replace(/[^a-zA-Z0-9 ]/g, '').trim()})
-                axios.post("https://search.findmanual.guru/manual/search/insert", {id : $(finalObject[i]).children("a").text().replace(/[^a-zA-Z0-9 ]/g, '').trim().replaceAll(' ', '_') , brand, category, "url":  "http://www.manualsonline.com" + $(finalObject[i]).children("a").attr('href'), "title": $(finalObject[i]).children("a").text().replace(/[^a-zA-Z0-9 ]/g, '').trim()})
+
+                manualSchema.addManual({brand, category, "url":  "http://www.manualsonline.com" + $(finalObject[i]).children("a").attr('href'), "title": $(finalObject[i]).children("a").text().replace(/[^a-zA-Z0-9 ]/g, '').trim()})
                     .then(data => console.log("ok " + i))
-                    .catch(e => console.log(e));
+                    .catch(err => console.error(err))
             }
 
-            resolve({message : "done3"});
+            resolve({message : "done", isForDb : true});
 
         } catch (e) {
-            resolve({message : "done3"});
+            resolve({message : "done", isForDb : true});
         }
     })
 
@@ -101,18 +111,20 @@ function getThirdData(obj) {
 
 
 parentPort.on('message', async (message) => {
-    if (message.message === "first") {
-        const data = await getFirstData(message);
-        parentPort.postMessage(data);
-    }
-    if (message.message === "second") {
-        const data = await getSecondData(message);
-        parentPort.postMessage(data);
-    }
+    if (message.message === "run") {
+        if (message.type === "brand") {
+            const data = await getFirstData(message);
+            parentPort.postMessage(data);
+        }
 
-    if (message.message === "third") {
-        const data = await getThirdData(message);
-        console.log(data);
-        parentPort.postMessage(data);
+        if (message.type === "category") {
+            const data = await getSecondData(message);
+            parentPort.postMessage(data);
+        }
+
+        if (message.type === "lastUrl") {
+            const data = await getThirdData(message);
+            parentPort.postMessage(data);
+        }
     }
 });
